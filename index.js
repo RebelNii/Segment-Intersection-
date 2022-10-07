d937fe47d5d1659aa81a4ad7859e80d901f9601e
Kwade Jeffrey <kwadejeffrey@gmail.com>
2022-10-07 17:35:32 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+canvas = document.getElementById("myCanvas");
+
+let t = 0;
+
+const c = canvas.getContext("2d");
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth * 0.9;
+
+//c.fillRect(0,0,canvas.width,canvas.height);
+
+function drawLabel(point, label, isInter) {
+  c.beginPath();
+  c.fillStyle =isInter?"red" :"white";
+  c.arc(point.x, point.y, 10, 0, Math.PI * 2);
+  c.fill();
+  c.stroke();
+  c.fillStyle = "black";
+  c.textAlign = "center";
+  c.textBaseline = "middle";
+  c.font = "bold 14px Arial";
+  c.fillText(label, point.x, point.y);
+}
+
+function lerp(A, B, t) {
+  return A + (B - A) * t;
+}
+
+function getIntersection(A,B,C,D){
+  /**
+   * Ix = Ax+(Bx-Ax)*t = CDx+(Dx-CDx)*u
+   * Iy = Ay+(By-Ay)*t = CDy+(Dy-CDy)*u
+   */
+
+  const top = (D.x - C.x)*(A.y-C.y)-(D.y-C.y)*(A.x-C.x);
+  const bottom = (D.y-C.y)*(B.x-A.x)-(D.x-C.x)*(B.y-A.y);
+  const t = top/bottom;
+  console.log(t)
+  return {
+    x: lerp(A.x,B.x,t),
+    y:lerp(A.y,B.y,t)
+  }
+}
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  const A = { x: 200, y: 150 };
+  const B = { x: 150, y: 250 };
+  const CD = { x: 50, y: 100 };
+  const D = { x: 250, y: 200 };
+
+  const M = {
+    x: lerp(A.x, B.x, t),
+    y: lerp(A.y, B.y, t),
+  };
+
+  const F = {
+    x: lerp(CD.x, D.x, t),
+    y: lerp(CD.y, D.y, t),
+  };
+  c.lineWidth = 5;
+  c.strokeStyle = "yellow";
+  c.beginPath();
+  c.moveTo(A.x, A.y);
+  c.lineTo(B.x, B.y);
+  c.moveTo(CD.x, CD.y);
+  c.lineTo(D.x, D.y);
+  c.stroke();
+
+  drawLabel(A, "A");
+  drawLabel(B, "B");
+  drawLabel(CD, "CD");
+  drawLabel(D, "D");
+  drawLabel(M, "M", t<0 || t>1);
+  drawLabel(F, "F",t<0 || t>1);
+
+  const I = getIntersection(A,B,CD,D);
+
+  drawLabel(I,"I")
+
+  //t += 0.005;
+}
+
+animate();
+
+/*
+c.beginPath();
+c.fillStyle = "white";
+c.arc(A.x, A.y, 10, 0, Math.PI * 2);
+c.fill();
+c.stroke();
+c.fillStyle = "black";
+c.textAlign = "center";
+c.textBaseline = "middle";
+c.font = "bold 14px Arial";
+c.fillText("A", A.x, A.y);*/
